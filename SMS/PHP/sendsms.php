b31421f24e81ccf3089e4bf77151e3cb3fadb70b
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2022-12-15 03:49:18 +0300

    changing sms format

--- SMS/PHP/sendsms.php
+++ SMS/PHP/sendsms.php
@@ -1,22 +1,17 @@
 <?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://api.umeskiasoftwares.com/api/v1/sms',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => '{
-        "api_key":"Repalace here you with your key",
-        "email":"example@gmail.com",
-        "Sender_Id": "23107",
-        "message": "Welcome to my company",
-        "phone":"2547XXXXXX60"
-      }',
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json'
-    ),
-));
-
-$response = curl_exec($curl);
-
-echo $response;
+  $Url = "https://api.umeskiasoftwares.com/api/v1/sms";
+  $ch = curl_init($Url);
+  $data = array(
+    'api_key' => 'SVQ4WVpPQUQ6NnpicDJsMm8=',
+    'email' => 'example@gmail.com',
+    'Sender_Id' => '23107',
+    'message' => "076***8060",
+    'phone' => "testing",
+  );
+  $payload = json_encode($data);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Accept:application/json'));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  
